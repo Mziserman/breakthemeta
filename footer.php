4989d005272adf9ca0e8eaa023ff7edc1fdf7466
AntoineMonni <antoine.monnier@hetic.net>
2015-10-21 18:25:31 +0200

    Home page + footer

--- footer.php
+++ footer.php
@@ -1,26 +1,41 @@
  <!-- Footer Area -->
 
 	<div class="footer-contain"><!-- Begin Footer -->
-    	<div class="container">
-            <div class="row"><!-- Begin Sub Footer -->
-                <div class="span12 footer-col footer-sub">
-                    <div class="row no-margin">
-                        <div class="span6">
-                            <span class="left">Copyright 2015 HETIC Wordpress training. All rights reserved.</span>
-                        </div>
-                        <div class="span6">
-                            <span class="right">
-                            <a href="index.htm">Accueil</a> | <a href="projects.htm">Projets</a> | <a href="gallery-4col.htm">Films</a> | <a href="blog-style2.htm">Blog</a> | <a href="page-contact.htm">Contact</a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div><!-- End Sub Footer -->
-        </div>
-    </div><!-- End Footer --> 
-    
-    <!-- Scroll to Top -->  
-    <div id="toTop" class="hidden-phone hidden-tablet">Remonter</div>
+  	<div class="footer-logo">
+      <p>NewMeta</p>   
+    </div>
+
+    <div class="footer-about">
+      <p class="footer-about-title">
+        About us :
+      </p>
+      <p class="footer-about-text">Hi fellows, we are 5 students who just want to have fun on league of legends. </p>
+      <p class="footer-about-text">So we create <span class="footer-about-text--bold">NEWMETA</span> for people who love break the meta like us.  </p>
+    </div>
+
+    <div class="footer-menu">
+      <div class="footer-menu-top">
+        <p class="footer-menu-title">Jump to :</p>
+        <ul class="footer-menu-list">
+          <li class="footer-menu-item"><a href="#">Landing</a></li>
+          <li class="footer-menu-item"><a href="<?php echo get_site_url(); ?>/build/">Buildlist</a></li>
+          <li class="footer-menu-item"><a href="#">Create a new build</a></li>
+          <li class="footer-menu-item"><a href="#">Profil</a></li>
+        </ul>
+      </div>
+      
+      <p class="footer-menu-copyright">© 2015 NEWMETA</p>
+    </div>
+
+    <div class="footer-share">
+      <p class="footer-share-text">Connect with us !</p>
+      <div class="footer-share-icons">
+        <a href="#" class="footer-share-icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/fb-icon.png" alt="">
+        </a>
+      </div>
+    </div>
+  </div><!-- End Footer --> 
 
     <?php wp_footer(); ?>
 </body>
--- index.php
+++ index.php
@@ -2,7 +2,12 @@
     
 	<div class="container home-container">
 
-		Hello home page maggle
+		<h1 class="home-container-title">NewMeta</h1>
+		<p class="home-container-desc">Find the best build to break the meta or create a new one</p>
+
+		<a href="<?php echo get_site_url(); ?>/build/" class="home-container-btn">
+			<p class="home-container-btn-text">Buildlist</p>
+		</a>
 		
 	</div>
 	
