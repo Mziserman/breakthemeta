e82f252f80ae0a1c2c1bd2bebe742c31fddf062b
stephenrichard <stephen.richard44@gmail.com>
2015-10-21 18:10:59 +0200

    Add pagination/order by + fix some css

--- archive-build.php
+++ archive-build.php
@@ -42,7 +42,7 @@
     				</div>
     			</div>
     			<div class="lanes">
-    				<a class="title" href="">Lanes<i class="fa fa-plus"></i></a>
+    				<a class="title" href="">Lanes<!-- <i class="fa fa-plus"></i> --><span class="plus"></span></a>
     				<div class="filter-list">
     					<ul>
     						<li><a href="">Toplane</a></li>
@@ -53,7 +53,7 @@
     				</div>
     			</div>
     			<div class="roles">
-    				<a class="title" href="">Roles<i class="fa fa-plus"></i></a>
+    				<a class="title" href="">Roles<!-- <i class="fa fa-plus"></i> --><span class="plus"></span></a>
     				<div class="filter-list">
     					<ul>
     						<li><a href="">Carry AP</a></li>
@@ -83,60 +83,100 @@
                         <li><a href="#panel-2">Top rated</a></li>
                         <li><a href="#panel-3">Debated</a></li>
                     </ul>
-                    <ul class="page-number">
-                        <li class="active"><a href="">1</a></li>
-                        <li><a href="">2</a></li>
-                        <li><a href="">3</a></li>
-                        <li><a href="">31</a></li>
-                        <li><a href="">Next</a></li>
-                    </ul>
                 </div>
                 <div class="build-list-content">
-                    <div class="panel show" id="panel-1"><?php if(have_posts()) :  ?>
+                    <div class="panel show" id="panel-1">
                         <ul>
+                            <?php
+                                $loop = new WP_Query( array( 'posts_per_page' => 10, 'post_type' => 'build', 'orderby' => 'date') );
+                                if ( $loop->have_posts() ) :
+                                    while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                                         <li class="blog-build-item">
+                                            <img class="champion-portrait" src="<?php echo get_template_directory_uri(); ?>/img/champion/Azir.png" alt="">
 
-                        <?php while ( have_posts() ) : the_post(); ?>
-                            <!-- Si j'ai des posts, j'affiche cette partie -->
-
-                            <li class="blog-build-item">
-                                <img class="champion-portrait" src="<?php echo get_template_directory_uri(); ?>/img/champion/Azir.png" alt="">
-
-                                <div class="author-and-date">
-                                    <a class="build-link" href="<?php the_permalink(); ?>" class="clearfix"><?php the_title(); ?></a>
-                                    
-                                    <?php 
-                                        $dateformatstring = 'd F Y';
-                                        $unixtimestamp = strtotime(get_field('date_de_publication'));
-                                    ?>
-                                    <p>by <a href="">Popolopo26</a> - <?php echo date_i18n($dateformatstring, $unixtimestamp); ?></p>
-                                </div>
-                                <div class="items-list">
-                                    <ul>
-                                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Elise.png" alt=""></a></li>
-                                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Draven.png" alt=""></a></li>
-                                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Ezreal.png" alt=""></a></li>
-                                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Anivia.png" alt=""></a></li>
-                                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Annie.png" alt=""></a></li>
-                                        <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Brand.png" alt=""></a></li>
-                                    </ul>
-                                </div>
-                                
-                                <div class="likes-and-comments">
-                                    <span><img src="<?php echo get_template_directory_uri(); ?>/img/heart.png" alt=""><p>15</p></span>
-                                    <span><img src="<?php echo get_template_directory_uri(); ?>/img/comment-bubble.png" alt=""><p>6</p></span>
-                                </div>
-                            </li>
-                            
-                            
-                        <?php endwhile; ?>
-
+                                            <div class="author-and-date">
+                                                <a class="build-link" href="<?php the_permalink(); ?>" class="clearfix"><?php the_title(); ?></a>
+                              
+                                                <p>by <a href="">Popolopo26</a> - <?php echo get_the_date(); ?></p>
+                                            </div>
+                                            <div class="items-list">
+                                                <ul>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Elise.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Draven.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Ezreal.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Anivia.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Annie.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Brand.png" alt=""></li>
+                                                </ul>
+                                            </div>
+                                            
+                                            <div class="likes-and-comments">
+                                                <span><img src="<?php echo get_template_directory_uri(); ?>/img/heart.png" alt=""><p>15</p></span>
+                                                <span><img src="<?php echo get_template_directory_uri(); ?>/img/comment-bubble.png" alt=""><p>6</p></span>
+                                            </div>
+                                        <li>
+                                    <?php endwhile;
+                                    if (  $loop->max_num_pages > 1 ) : ?>
+                                        <div class="nav-below" class="navigation">
+                                            <?php echo paginate_links(array(
+                                                'total' => 2,
+                                                'prev_next' => true,
+                                                'next_next' => __('Next'),
+                                                'total' => $wp_query->max_num_pages
+                                            )); ?>
+                                        </div>
+                                    <?php endif;
+                                endif;
+                                wp_reset_postdata();
+                            ?>
                         </ul>
-
-                        <?php else: ?>
-                            <p>Rien à afficher</p>
-                        <?php endif; ?>
                     </div>
-                    <div class="panel" id="panel-2">Top rated builds list</div>
+                    <div class="panel" id="panel-2">
+                        <ul>
+                            <?php
+                                $loop = new WP_Query( array( 'posts_per_page' => 10, 'post_type' => 'build', 'orderby' => 'rand') );
+                                if ( $loop->have_posts() ) :
+                                    while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                                         <li class="blog-build-item">
+                                            <img class="champion-portrait" src="<?php echo get_template_directory_uri(); ?>/img/champion/Azir.png" alt="">
+
+                                            <div class="author-and-date">
+                                                <a class="build-link" href="<?php the_permalink(); ?>" class="clearfix"><?php the_title(); ?></a>
+
+                                                <p>by <a href="">Popolopo26</a> - <?php echo get_the_date(); ?></p>
+                                            </div>
+                                            <div class="items-list">
+                                                <ul>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Elise.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Draven.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Ezreal.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Anivia.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Annie.png" alt=""></li>
+                                                    <li><img src="<?php echo get_template_directory_uri(); ?>/img/champion/Brand.png" alt=""></li>
+                                                </ul>
+                                            </div>
+                                            
+                                            <div class="likes-and-comments">
+                                                <span><img src="<?php echo get_template_directory_uri(); ?>/img/heart.png" alt=""><p>15</p></span>
+                                                <span><img src="<?php echo get_template_directory_uri(); ?>/img/comment-bubble.png" alt=""><p>6</p></span>
+                                            </div>
+                                        <li>
+                                    <?php endwhile;
+                                    if (  $loop->max_num_pages > 1 ) : ?>
+                                        <div class="nav-below" class="navigation">
+                                            <?php echo paginate_links(array(
+                                                'total' => 2,
+                                                'prev_next' => true,
+                                                'next_next' => __('Next'),
+                                                'total' => $wp_query->max_num_pages
+                                            )); ?>
+                                        </div>
+                                    <?php endif;
+                                endif;
+                                wp_reset_postdata();
+                            ?>
+                        </ul>
+                    </div>
                     <div class="panel" id="panel-3">Debated build list</div>
                 </div>
             </div>
