0a4747e12dbc945d66f3e5d24fdcb1cbc6189259
Mziserman <martinziserman@gmail.com>
2015-10-28 00:06:05 +0100

    working pagination and panels switchs

--- js/ajax_script.js
+++ js/ajax_script.js
@@ -1,34 +1,4 @@
 //Objects definitions
-var Panel = function($, name, button, container) {
-	this.init($, name, button, container);
-}
-Panel.prototype = {
-	init: function($, name, button, container) {
-		this.$ = $;
-		this.name = name;
-		this.button = button;
-		this.container = container;
-		this.pagination = new Pagination(10);
-		this.active = this.name === 'date';
-		this.visited = this.name === 'date';
-		this.addInteractions()
-	},
-	show: function() {
-		this.button.addClass('active')
-		this.container.removeClass('hide').addClass('show');
-		this.button.addClass
-		this.active = true;
-	},
-	hide: function() {
-		this.button.removeClass('active')
-		this.container.removeClass('show').addClass('hide');
-		this.active = false;
-	},
-	addInteractions: function() {
-
-	}
-}
-
 var CurrentState = function($){
 	this.init($)
 }
@@ -81,6 +51,8 @@ CurrentState.prototype = {
 				that.getBuilds();
 			}
 		});
+
+
 	},
 	getBuilds: function(){
 		var orderBy = this.currentPanel.name;
@@ -97,6 +69,88 @@ CurrentState.prototype = {
 		    },
 		    function(response){
 		    	that.currentPanel.container.prepend(response);
+	        }
+		);
+	}
+}
+
+var Panel = function($, name, button, container) {
+	this.init($, name, button, container);
+}
+Panel.prototype = {
+	init: function($, name, button, container) {
+		this.$ = $;
+		
+		this.name = name;
+		
+		this.button = button;
+		this.container = container;
+		
+		this.pagination = new Pagination(10);
+		
+		this.active = this.name === 'date';
+		this.visited = this.name === 'date';
+
+		this.addInteractions()
+	},
+	show: function() {
+		this.button.addClass('active')
+		this.container.removeClass('hide').addClass('show');
+		this.button.addClass
+		this.active = true;
+	},
+	hide: function() {
+		this.button.removeClass('active')
+		this.container.removeClass('show').addClass('hide');
+		this.active = false;
+	},
+	setPageMaxAndPrintPagination: function(pageMax) {
+		this.pagination.pageMax = pageMax;
+		this.printPaginationAndAddInteraction();
+	},
+	printPaginationAndAddInteraction: function() {
+		var nav_container = this.$('<div>').addClass('nav-below');
+		for (var i = 1; i <= this.pagination.pageMax; i++){
+			if (i == this.pagination.currentPage){
+				nav_container.append(this.$('<span>').addClass('page-numbers').addClass('current').html(i))
+			} else {
+				nav_container.append(this.$('<a>').addClass('page-numbers').attr('href', i).html(i));	
+			}
+		}
+		this.container.append(nav_container);
+		this.addInteractions();
+	},
+	addInteractions: function() {
+		var navContainer = this.container.find('.nav-below');
+		var that = this;
+		navContainer.on('click', 'a', function(e) {
+			e.preventDefault();
+			that.pagination.setPage(that.$(this).html());
+
+			var exCurrentPage = navContainer.find('span');
+			exCurrentPage.replaceWith(that.$('<a>').addClass('page-numbers').attr('href', exCurrentPage.html()).html(exCurrentPage.html()));
+			var currentPage = navContainer.find("a[href=" + that.pagination.currentPage + "]")
+			currentPage.replaceWith(that.$('<span>').addClass('page-numbers').addClass('current').html(that.pagination.currentPage))
+
+			that.getBuilds();
+		})
+	}, 
+	getBuilds: function() {
+		var orderBy = this.name;
+		var offset = this.pagination.offset;
+		var posts_per_page = this.pagination.postsPerPage;
+		var that = this;
+		this.$.post(
+		    ajaxurl,
+		    {
+		        'action': 'get_builds',
+		        'offset': offset,
+		        'posts_per_page': posts_per_page,
+		        'orderby': orderBy,
+		    },
+		    function(response){
+		    	that.container.find('.blog-build-item').remove();
+		    	that.container.prepend(response);
 	        }
 		);
 	}
@@ -133,13 +187,13 @@ jQuery(document).ready(function($){
 		    },
 		    function(response){
 		    	pagination.pageMax = Math.ceil(response / pagination.postsPerPage);
-		    	currentState.panels.date.pagination.pageMax = Math.ceil(response / pagination.postsPerPage);
-		    	currentState.panels.rand.pagination.pageMax = Math.ceil(response / pagination.postsPerPage);
-		    	currentState.panels.debated.pagination.pageMax = Math.ceil(response / pagination.postsPerPage);
+		    	currentState.panels.date.setPageMaxAndPrintPagination(Math.ceil(response / pagination.postsPerPage));
+		    	currentState.panels.rand.setPageMaxAndPrintPagination(Math.ceil(response / pagination.postsPerPage));
+		    	currentState.panels.debated.setPageMaxAndPrintPagination(Math.ceil(response / pagination.postsPerPage));
 		    }
 	    )
 	).then(function(){
-		printPagination($, pagination, currentState);
+		// printPagination($, pagination, currentState);
 		// addInteractions($, pagination, currentState);
 		console.log(currentState)
 	})
