1d6666ef773d4c663938d26d7872569dcc6491d7
stephenrichard <stephen.richard44@gmail.com>
2015-11-04 00:03:12 +0100

    Add page 404 + minimal responsive menu + home and 404 button centering

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+	<div class="container notfound-container">
+
+		<h1 class="home-container-title">It's a trap !</h1>
+		<p class="home-container-desc">Teemo trapped you in a 404 ! You have to escape ! </p>
+
+		<a href="<?php echo get_site_url(); ?>/build/" class="home-container-btn">
+			Run to home page
+		</a>
+
+	</div>
+
+<?php get_footer(); ?>
