ec10ed7de1c355095c2724447218ac6ac9cf2068
stephenrichard <stephen.richard44@gmail.com>
2015-11-04 12:06:23 +0100

    Update build header background URL online (may not work locally)

--- template-summary-header-background.php
+++ template-summary-header-background.php
@@ -1,5 +1,7 @@
 <?php 
 	$championID = get_post_meta(get_the_ID() ,'champion', true);
 	$splash_art = get_field_object('splash_art', $championID)['value'];
+
+	$upload_dir = wp_upload_dir();
 ?>
-<img class="build-header-bg" src="<?php echo $splash_art['url']; ?>" alt="">
+<img class="build-header-bg" src="<?php echo $upload_dir['baseurl']; ?>/2015/11/<?php echo get_the_title($championID); ?>_0.jpg" alt="">
