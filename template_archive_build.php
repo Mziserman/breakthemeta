6b49f41f76489ca10c93f316b483cf9af7ca05ae
Mziserman <martinziserman@gmail.com>
2015-11-04 16:08:57 +0100

    fixed beginstuff, and working button like and dynamic likes show

--- template_archive_build.php
+++ template_archive_build.php
@@ -1,5 +1,6 @@
 <?php 
     $champion = get_field_object('champion')['value'];
+    $score = get_field_object('likes')['value'];
 
 ?>
 
@@ -17,7 +18,7 @@
     </div>
     
     <div class="likes-and-comments">
-        <span><img src="<?php echo get_template_directory_uri(); ?>/img/heart.png" alt=""><p>15</p></span>
+        <span><img src="<?php echo get_template_directory_uri(); ?>/img/heart.png" alt=""><p><?php echo $score; ?></p></span>
         <span><img src="<?php echo get_template_directory_uri(); ?>/img/comment-bubble.png" alt=""><p><?php echo get_comments_number(); ?></p></span>
     </div>
 </li>
