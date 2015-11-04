0831a59d6c5dc493f21cfee79db79bd579973634
BorisLaporte <boris.laporte@gmail.com>
2015-11-04 16:41:42 +0100

    security check on create-build.php

--- create-build.php
+++ create-build.php
@@ -9,149 +9,186 @@
   	$cb = new Create_build($wpdb);
   	if ( !empty($_POST) )
   	{
-  		$image_id = get_post_thumbnail_id( $_POST['champ'] );
 
-  		$post_id = $cb->create_post($_POST['title'],$_POST['description'],'build',$_POST['excerpt'],$image_id);
-
-  		if ( !empty($post_id) )
+  		if ( empty($cb->handleErrors($_POST)) )
   		{
-  			// champion
-			update_field("champion", $_POST['champ'], $post_id);
+  			$image_id = get_post_thumbnail_id( $_POST['champ'] );
 
-  			// lane
-  			wp_set_object_terms( $post_id, $_POST['lane'], 'lane');
+	  		$post_id = $cb->create_post($_POST['title'],$_POST['description'],'build',$_POST['excerpt'],$image_id);
 
-  			// role
-  			wp_set_object_terms( $post_id, $_POST['role'], 'role');
+	  		if ( !empty($post_id) )
+	  		{
+	  			// champion
+				update_field("champion", $_POST['champ'], $post_id);
 
-  			// begin item
-  			$begin_item = get_field(field_5630ea67f0b4b, $post_id);
-	  		for ( $i = 1; $i <= 6; $i++ )
-		  	{			
-		  		if ( !empty($_POST['begin-item-'.$i]) )
-		  		{
-					$begin_item[] = array("items" => $_POST['begin-item-'.$i]);		
-		  		}
-		  	}
-		  	update_field( field_5630ea67f0b4b, $begin_item, $post_id );
+	  			// lane
+	  			wp_set_object_terms( $post_id, $_POST['lane'], 'lane');
 
-  			// end item
-  			$end_item = get_field(field_5630ebc3f0b4c, $post_id);
-	  		for ( $i = 1; $i <= 6; $i++ )
-		  	{			
-		  		if ( !empty($_POST['end-item-'.$i]) )
-		  		{
-					$end_item[] = array("items" => $_POST['end-item-'.$i]);		
-		  		}
-		  	}
-		  	update_field( field_5630ebc3f0b4c, $end_item, $post_id );
+	  			// role
+	  			wp_set_object_terms( $post_id, $_POST['role'], 'role');
 
-
-		  	// summoner spell
-  			$summoner = get_field(field_5630e250b0a69, $post_id);
-	  		for ( $i = 1; $i <= 2; $i++ )
-		  	{			
-		  		if ( !empty($_POST['summoner-'.$i]) )
-		  		{
-					$summoner[] = array("spell" => $_POST['summoner-'.$i]);		
-		  		}
-		  	}
-		  	update_field( field_5630e250b0a69, $summoner, $post_id );
-
-
-		  	// spell order
-	  		$spell_order = get_field(field_563788d6796f6, $post_id);
-	  		for ( $i = 1; $i <= 18; $i ++ )
-		  	{			
-		  		for ( $j = 1; $j <= 5; $j ++)
-		  		{
-			  		if ( !empty($_POST['order-input-'.$j.'-'.$i]) && $_POST['order-input-'.$j.'-'.$i] == 1 )
+	  			// begin item
+	  			$begin_item = get_field(field_5630ea67f0b4b, $post_id);
+		  		for ( $i = 1; $i <= 4; $i++ )
+			  	{			
+			  		if ( !empty($_POST['begin-item-'.$i]) )
 			  		{
-						$spell_order[] = array("line" => $j );		
+						$begin_item[] = array("items" => $_POST['begin-item-'.$i]);		
 			  		}
-		  		}
-		  	}
-		  	update_field( field_563788d6796f6, $spell_order, $post_id );
+			  	}
+			  	update_field( field_5630ea67f0b4b, $begin_item, $post_id );
 
-  		
-
-	  		// MARKS
-	  		$get_marks = [];
-	  		for ( $i =1; $i <= 9; $i ++ )
-	  		{
-	  			$get_marks[] = $_POST['red-runes-'.$i];
-	  		}
-	  		$marks_sorted = $cb->preset_runes($get_marks);
-
-	  		$marks = get_field(field_5631ecf03daf7, $post_id);
-	  		for ( $i = 0; $i < count($marks_sorted); $i ++ )
-	  		{
-	  			if ( !empty($marks_sorted[$i]['rune']) )
+	  			// end item
+	  			$end_item = get_field(field_5630ebc3f0b4c, $post_id);
+		  		for ( $i = 1; $i <= 6; $i++ )
+			  	{			
+			  		if ( !empty($_POST['end-item-'.$i]) )
 			  		{
-						$marks[] = array("mark" => $marks_sorted[$i]['rune'], 'quantity' => $marks_sorted[$i]['quantity']);		
+						$end_item[] = array("items" => $_POST['end-item-'.$i]);		
 			  		}
-	  		}
-	  		update_field( field_5631ecf03daf7, $marks, $post_id );
+			  	}
+			  	update_field( field_5630ebc3f0b4c, $end_item, $post_id );
 
-	  		// GLYPHS
-	  		$get_glyphs = [];
-	  		for ( $i =1; $i <= 9; $i ++ )
-	  		{
-	  			$get_glyphs[] = $_POST['blue-runes-'.$i];
-	  		}
-	  		$glyphs_sorted = $cb->preset_runes($get_glyphs);
 
-	  		$glyphs = get_field(field_5631edb53dafd, $post_id);
-	  		for ( $i = 0; $i < count($glyphs_sorted); $i ++ )
-	  		{
-	  			if ( !empty($glyphs_sorted[$i]['rune']) )
+			  	// summoner spell
+	  			$summoner = get_field(field_5630e250b0a69, $post_id);
+		  		for ( $i = 1; $i <= 2; $i++ )
+			  	{			
+			  		if ( !empty($_POST['summoner-'.$i]) )
 			  		{
-						$glyphs[] = array("glyph" => $glyphs_sorted[$i]['rune'], 'quantity' => $glyphs_sorted[$i]['quantity']);		
+						$summoner[] = array("spell" => $_POST['summoner-'.$i]);		
 			  		}
-	  		}
-	  		update_field( field_5631edb53dafd, $glyphs, $post_id );
+			  	}
+			  	update_field( field_5630e250b0a69, $summoner, $post_id );
 
-	  		// SEALS
-	  		$get_seals = [];
-	  		for ( $i =1; $i <= 9; $i ++ )
-	  		{
-	  			$get_seals[] = $_POST['yellow-runes-'.$i];
-	  		}
-	  		$seals_sorted = $cb->preset_runes($get_seals);
 
-	  		$seals = get_field(field_5631ed3d3dafb, $post_id);
-	  		for ( $i = 0; $i < count($seals_sorted); $i ++ )
-	  		{
-	  			if ( !empty($seals_sorted[$i]['rune']) )
+			  	// spell order
+		  		$spell_order = get_field(field_563788d6796f6, $post_id);
+		  		for ( $i = 1; $i <= 18; $i ++ )
+			  	{			
+			  		for ( $j = 1; $j <= 5; $j ++)
 			  		{
-						$seals[] = array("seal" => $seals_sorted[$i]['rune'], 'quantity' => $seals_sorted[$i]['quantity']);		
+				  		if ( !empty($_POST['order-input-'.$j.'-'.$i]) && $_POST['order-input-'.$j.'-'.$i] == 1 )
+				  		{
+							$spell_order[] = array("line" => $j );		
+				  		}
 			  		}
-	  		}
-	  		update_field( field_5631ed3d3dafb, $seals, $post_id );
+			  	}
+			  	update_field( field_563788d6796f6, $spell_order, $post_id );
 
-	  		// QUINTE
-	  		$get_quintes = [];
-	  		for ( $i =1; $i <= 9; $i ++ )
-	  		{
-	  			$get_quintes[] = $_POST['black-runes-'.$i];
-	  		}
-	  		$quintes_sorted = $cb->preset_runes($get_quintes);
-
-	  		$quintes = get_field(field_5631e6a4768f7, $post_id);
-	  		for ( $i = 0; $i < count($quintes_sorted); $i ++ )
-	  		{
-	  			if ( !empty($quintes_sorted[$i]['rune']) )
-			  		{
-						$quintes[] = array("quintessence" => $quintes_sorted[$i]['rune'], 'quantity' => $quintes_sorted[$i]['quantity']);		
-			  		}
-	  		}
-	  		update_field( field_5631e6a4768f7, $quintes, $post_id );
 	  		
-	  		$url = get_post_permalink($post_id);
-	  		wp_redirect($url);
-			exit();
-  		}
 
+		  		// MARKS
+		  		$get_marks = [];
+		  		for ( $i =1; $i <= 9; $i ++ )
+		  		{
+		  			$get_marks[] = $_POST['red-runes-'.$i];
+		  		}
+		  		$marks_sorted = $cb->preset_runes($get_marks);
+
+		  		$marks = get_field(field_5631ecf03daf7, $post_id);
+		  		for ( $i = 0; $i < count($marks_sorted); $i ++ )
+		  		{
+		  			if ( !empty($marks_sorted[$i]['rune']) )
+				  		{
+							$marks[] = array("mark" => $marks_sorted[$i]['rune'], 'quantity' => $marks_sorted[$i]['quantity']);		
+				  		}
+		  		}
+		  		update_field( field_5631ecf03daf7, $marks, $post_id );
+
+		  		// GLYPHS
+		  		$get_glyphs = [];
+		  		for ( $i =1; $i <= 9; $i ++ )
+		  		{
+		  			$get_glyphs[] = $_POST['blue-runes-'.$i];
+		  		}
+		  		$glyphs_sorted = $cb->preset_runes($get_glyphs);
+
+		  		$glyphs = get_field(field_5631edb53dafd, $post_id);
+		  		for ( $i = 0; $i < count($glyphs_sorted); $i ++ )
+		  		{
+		  			if ( !empty($glyphs_sorted[$i]['rune']) )
+				  		{
+							$glyphs[] = array("glyph" => $glyphs_sorted[$i]['rune'], 'quantity' => $glyphs_sorted[$i]['quantity']);		
+				  		}
+		  		}
+		  		update_field( field_5631edb53dafd, $glyphs, $post_id );
+
+		  		// SEALS
+		  		$get_seals = [];
+		  		for ( $i =1; $i <= 9; $i ++ )
+		  		{
+		  			$get_seals[] = $_POST['yellow-runes-'.$i];
+		  		}
+		  		$seals_sorted = $cb->preset_runes($get_seals);
+
+		  		$seals = get_field(field_5631ed3d3dafb, $post_id);
+		  		for ( $i = 0; $i < count($seals_sorted); $i ++ )
+		  		{
+		  			if ( !empty($seals_sorted[$i]['rune']) )
+				  		{
+							$seals[] = array("seal" => $seals_sorted[$i]['rune'], 'quantity' => $seals_sorted[$i]['quantity']);		
+				  		}
+		  		}
+		  		update_field( field_5631ed3d3dafb, $seals, $post_id );
+
+		  		// QUINTE
+		  		$get_quintes = [];
+		  		for ( $i =1; $i <= 9; $i ++ )
+		  		{
+		  			$get_quintes[] = $_POST['black-runes-'.$i];
+		  		}
+		  		$quintes_sorted = $cb->preset_runes($get_quintes);
+
+		  		$quintes = get_field(field_5631e6a4768f7, $post_id);
+		  		for ( $i = 0; $i < count($quintes_sorted); $i ++ )
+		  		{
+		  			if ( !empty($quintes_sorted[$i]['rune']) )
+				  		{
+							$quintes[] = array("quintessence" => $quintes_sorted[$i]['rune'], 'quantity' => $quintes_sorted[$i]['quantity']);		
+				  		}
+		  		}
+		  		update_field( field_5631e6a4768f7, $quintes, $post_id );
+		  		
+		  		$url = get_post_permalink($post_id);
+		  		wp_redirect($url);
+				exit();
+  			}
+  		} else {
+  			$errors = $cb->handleErrors($_POST);
+  			if ( !empty($errors['begin-item']) || !empty($errors['end-item']) )
+  			{
+  				$errors['location'][] = "items"; 
+  			}
+  			if ( !empty($errors['spell-order']) )
+  			{
+  				$errors['location'][] = "spell order"; 
+  			}
+  			if ( !empty($errors['summoner']) )
+  			{
+  				$errors['location'][] = "summoner"; 
+  			}
+  			if ( !empty($errors['marks']) || !empty($errors['seals']) || !empty($errors['glyphs']) || !empty($errors['quintes']) )
+  			{
+  				$errors['location'][] = "runes"; 
+  			}
+
+  			if ( isset($errors['location']) && !empty($errors['location']) )
+  			{
+  				$errors['where'] = "You have errors in";
+  				$i = 1;
+  				foreach ($errors['location'] as $location) {
+  					if ( $i == ( count($errors['location']) - 1 ) )
+  						$errors['where'] .= " ".$location. " and";
+					else if ( $i == ( count($errors['location']) ) )
+  						$errors['where'] .= " ".$location. ".";
+  					else
+  						$errors['where'] .= " ".$location. ",";
+
+  					$i ++;
+  				}
+  			}
+  		}
   	}
 
 	get_header();
@@ -171,19 +208,30 @@
 						<li><a href="#panel-2">Items</a></li>
 						<li><a href="#panel-3">Spells order</a></li>
 						<li><a href="#panel-4">Summoner spell</a></li>
-						<li><a href="#panel-5">Masteries</a></li>
+						<!-- <li><a href="#panel-5">Masteries</a></li> -->
 						<li><a href="#panel-6">Runes</a></li>
 						<li><a href="#panel-7">Gameplay</a></li>
 					</ul>
 				</div>
 				<div class="main create-build"  >
 					<a class="back-link" href="<?php echo get_site_url(); ?>/build/" class="pull-right"><i class="icon-arrow-left"></i>Back to builds</a>
-					<div class="panel" id="panel-1" champion>
+					<div class="panel show " id="panel-1" champion>
+						<div class="errors">
+							<?php if ( isset($errors['where']) && !empty($errors['where']) ) : ?>
+								<?php echo $errors['where'] ?>
+							<?php endif ?>
+						</div>
 						<div class="panel-section">
+							<?php if ( isset($errors['title']) && !empty($errors['title']) ) : ?>
+								<?php echo $errors['title'] ?>
+							<?php endif ?>
 							<h3>Guide title</h3>
 							<input type="text" name="title">
 						</div>
 						<div class="description">
+							<?php if ( isset($errors['excerpt']) && !empty($errors['excerpt']) ) : ?>
+								<?php echo $errors['excerpt'] ?>
+							<?php endif ?>
 							<h3>Short description</h3>
 							<textarea name="excerpt"></textarea>
 						</div>
@@ -191,6 +239,7 @@
 			    		<div class="lane">
 			              <h3>For which lane ?</h3>
 			              <select name="lane">
+			                <option value="all" >All</option>
 			                <option value="top" >Top</option>
 			                <option value="mid" >Mid</option>
 			                <option value="jungle" >Jungle</option>
@@ -209,6 +258,9 @@
 			            </div>
 						
 						<div class="champions">
+							<?php if ( isset($errors['champ']) && !empty($errors['champ']) ) : ?>
+								<?php echo $errors['champ'] ?>
+							<?php endif ?>
 							<h3>Champion</h3>
 
 
@@ -242,6 +294,9 @@
 					</div>
 
 					<div class="panel" id="panel-2" item>
+						<?php if ( isset($errors['begin-item']) && !empty($errors['begin-item']) ) : ?>
+								<?php echo $errors['begin-item'] ?>
+							<?php endif ?>
 						<h3>Starter item</h3>
 						<div class="begin-item">
 							<ul class="begin-end">
@@ -298,6 +353,9 @@
 		    				</div>
 
 						</div>
+						<?php if ( isset($errors['end-item']) && !empty($errors['end-item']) ) : ?>
+							<?php echo $errors['end-item'] ?>
+						<?php endif ?>
 						<h3>Core item</h3>
 						<div class="end-item">
 							<ul class="begin-end">
@@ -345,7 +403,7 @@
 				                            <?php if ( has_post_thumbnail() ) : ?>
 				                            	<span class="shadow"></span>
 				                            	<span><?php the_post_thumbnail('icon_liste'); ?></span>
-				                            	<?php if (preg_match('/^Enchantment', the_title())) : str_ireplace('Enchantment', '', the_title()); endif; ?></span>
+				                            	<?php if (preg_match('/^Enchantment/', the_title())) : str_ireplace('Enchantment', '', the_title()); endif; ?></span>
 				                            	<input type="hidden" value="<?php echo the_ID(); ?>"> 
 				                        	<?php endif ?>
 				                        </a></li>
@@ -358,7 +416,10 @@
 						</div>
 					</div>
 
-					<div class="panel show skill-order" id="panel-3" skill-order>
+					<div class="panel skill-order" id="panel-3" skill-order>
+						<?php if ( isset($errors['spell-order']) && !empty($errors['spell-order']) ) : ?>
+							<?php echo $errors['spell-order'] ?>
+						<?php endif ?>
 						<h3>Skill Order</h3>
 							<div class="panel-section">
 								<div class="so-header">
@@ -508,6 +569,9 @@
 					</div>
 					<div class="panel" id="panel-4" summoner>
 						<div class="summoner-spell">
+							<?php if ( isset($errors['summoner']) && !empty($errors['summoner']) ) : ?>
+								<?php echo $errors['summoner'] ?>
+							<?php endif ?>
 							<h3>Summoner spell</h3>
 							<ul>
 								<li class="item-chosen" summoner-chosen>
@@ -546,10 +610,13 @@
 					</div>
 
 					<div class="panel" id="panel-5">
-						<h3>Test</h3>
+						<h3>Not avaible for now</h3>
 					</div>
 
 					<div class="panel" id="panel-6" runes>
+						<?php if ( isset($errors['marks']) && !empty($errors['marks']) ) : ?>
+							<?php echo $errors['marks'] ?>
+						<?php endif ?>
 						<h3>Marks</h3>
 						<div class="runes-marks">
 							<ul class="chosen-runes">
@@ -613,6 +680,9 @@
 		    				</div>
 
 						</div>
+						<?php if ( isset($errors['seals']) && !empty($errors['seals']) ) : ?>
+							<?php echo $errors['seals'] ?>
+						<?php endif ?>
 						<h3>Seal</h3>
 						<div class="runes-seal">
 							<ul class="chosen-runes">
@@ -676,6 +746,9 @@
 		    				</div>
 
 						</div>
+						<?php if ( isset($errors['glyphs']) && !empty($errors['glyphs']) ) : ?>
+							<?php echo $errors['glyphs'] ?>
+						<?php endif ?>
 						<h3>Glyph</h3>
 						<div class="runes-glyph">
 							<ul class="chosen-runes">
@@ -739,6 +812,9 @@
 		    				</div>
 
 						</div>
+						<?php if ( isset($errors['quintes']) && !empty($errors['quintes']) ) : ?>
+							<?php echo $errors['quintes'] ?>
+						<?php endif ?>
 						<h3>Quint</h3>
 						<div class="runes-quinte">
 							<ul class="chosen-runes">
